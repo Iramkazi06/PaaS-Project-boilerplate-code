f4e89c90e806f5232f2e6d516546e4adbe1aa15a
Iram kazi <iramkazi06@gmail.com>
2026-04-09 12:25:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+
+function App() {
+  return (
+    <div style={{textAlign:'center', padding:'50px', fontFamily:'sans-serif'}}>
+      <h1>My Web App</h1>
+      <p>Deployed on Vercel using PaaS!</p>
+      <p style={{color:'#888'}}>Built with React + Vite</p>
+    </div>
+  )
+}
+export default App
